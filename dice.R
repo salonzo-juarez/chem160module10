f69bf4d62a5215a8b23d9627c94e484b4361935e
salonzo-juarez <54566713+salonzo-juarez@users.noreply.github.com>
2019-10-15 23:15:29 -0700

    Add files via upload

---
+++ dice.R
@@ -0,0 +1,13 @@
+if (!exists("ndice")) {
+  stop("You need to set ndice")
+}
+ndice <- 3
+rolls <- 1:6
+nrolls <- 10000
+total<-0
+for (i in 1:nrolls) {
+  roll<- sum(sort(sample(rolls,ndice,replace=TRUE), decreasing=TRUE)[1:2])
+  total<-total+roll
+}
+cat("Average score", total/nrolls, "\n")
+
